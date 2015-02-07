e1823f07af8eab0e245e199b410b795a230e1d8f
Gianni Moschini <gianni.moschini@hailocab.com>
2015-02-07 14:04:50 +0000

    More debug

--- files/brews/cassandra.rb
+++ files/brews/cassandra.rb
@@ -7,12 +7,17 @@ class Cassandra < Formula
   sha1 '217b3731784899f0228ea6f5bf98c9821e7f750e'
 
   def install
+    puts "creating config dif"
     (etc+"cassandra").mkpath
 
+    puts "replacing 1"
     inreplace "conf/cassandra.yaml", "/var/lib/cassandra", "#{var}/lib/cassandra"
+    puts "replacing 2"
     inreplace "conf/log4j-server.properties", "/var/log/cassandra", "#{var}/log/cassandra"
+    puts "replacing 3"
     inreplace "conf/cassandra-env.sh", "/lib/", "/"
 
+    puts "replacing 4"
     inreplace "bin/cassandra.in.sh" do |s|
       s.gsub! "CASSANDRA_HOME=\"`dirname $0`/..\"", "CASSANDRA_HOME=\"#{prefix}\""
       # Store configs in etc, outside of keg
@@ -21,10 +26,14 @@ class Cassandra < Formula
       s.gsub! "\"$CASSANDRA_HOME\"/lib/*.jar", "\"$CASSANDRA_HOME\"/*.jar"
     end
 
+    puts "deleting"
     rm Dir["bin/*.bat"]
 
+    puts "installing config"
     (etc+"cassandra").install Dir["conf/*"]
+    puts "installing txt files"
     prefix.install Dir["*.txt"] + Dir["{bin,interface,javadoc,pylib,lib/licenses}"]
+    puts "installing jar files"
     prefix.install Dir["lib/*.jar"]
   end
 end
